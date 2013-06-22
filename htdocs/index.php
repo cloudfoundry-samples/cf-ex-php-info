472194ee1e7cdb215b6b10ba6927011a7e6ae30f
Daniel Mikusa <dmikusa@gopivotal.com>
2013-06-21 23:39:34 -0400

    Updated php.ini, fixed added references to environment variabled.  Modified index to include a PHP file from the include path as a test.  Included test script in the app's include path.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,10 @@
+<?php
+    require('test.php');
+?>
 <html>
     <head><title>Hello World!</title></head>
     <body>
-        <h1>Hello World!</h1>
+        <h1><?php print helloWorld(); ?></h1>
         <p>Click <a href="/info.php">here to view PHP information</a></p>
     </body>
 </html>
---
+++ lib/test.php
@@ -0,0 +1,5 @@
+<?php
+    function helloWorld() {
+        return "Hello World!";
+    }
+?>
