19f99c4a68f41a1ff46e9397ef592361b6896340
Daniel Mikusa <dmikusa@gopivotal.com>
2013-07-19 21:47:29 -0400

    Added static resource

--- htdocs/index.php
+++ htdocs/index.php
@@ -6,5 +6,7 @@
     <body>
         <h1><?php print helloWorld(); ?></h1>
         <p>Click <a href="/info.php">here to view PHP information</a></p>
+        <p>Static resource test</p>
+        <img src="technical-difficulties1.jpg" alt="technical difficulties" />
     </body>
 </html>
