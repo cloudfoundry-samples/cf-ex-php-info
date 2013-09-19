25f5a9e342a30268c04686f9955e6119ca4204da
Daniel Mikusa <dmikusa@gopivotal.com>
2013-09-19 12:44:40 -0400

    Added param test, to check printing values from GET path parameters

--- htdocs/index.php
+++ htdocs/index.php
@@ -6,6 +6,7 @@
     <body>
         <h1><?php print helloWorld(); ?></h1>
         <p>Click <a href="/info.php">here to view PHP information</a></p>
+        <p>Click <a href="/params.php">here to view param test</a></p>
         <p>Static resource test</p>
         <img src="technical-difficulties1.jpg" alt="technical difficulties" />
     </body>
