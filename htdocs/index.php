ee3539f5fe8e91bd2333f0689508e9244bfac0da
Daniel Mikusa <dmikusa@gopivotal.com>
2014-03-04 16:35:09 -0500

    Added test for custom environment variable pass through.

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,5 +9,6 @@
         <p>Static resource test</p>
         <img src="technical-difficulties1.jpg" alt="technical difficulties" />
         <p>Property Test: <?php print (array_key_exists('test', $_GET) ? $_GET['test'] : 'None') ?> </p>
+        <p>Env Test: <?php print (array_key_exists('SOME_VAR', $_ENV) ? $_ENV['SOME_VAR'] : 'None') ?></p>
     </body>
 </html>
