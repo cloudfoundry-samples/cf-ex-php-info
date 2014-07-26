a722bf2d2145411f70d4cf12b516900a048fd363
Daniel Mikusa <dmikusa@gopivotal.com>
2014-07-26 13:28:13 -0400

    Removed cfenv, added git co to return options.json to known state.

--- test-push.sh
+++ test-push.sh
@@ -6,19 +6,19 @@ set -e
 git co .bp-config/options.json
 
 # push with Nginx
-cfenv pws
 cp .bp-config/options-nginx.json .bp-config/options.json
 cf push
 ./test-extensions.sh 5.4.31
 
 # push with HTTPD
-cfenv pws
 cp .bp-config/options-httpd.json .bp-config/options.json
 cf push
 ./test-extensions.sh 5.4.31
 
 # push with PHP 5.5
-cfenv pws
 cp .bp-config/options-php55.json .bp-config/options.json
 cf push
 ./test-extensions.sh 5.5.15
+
+# return to default
+git co .bp-config/options.json
