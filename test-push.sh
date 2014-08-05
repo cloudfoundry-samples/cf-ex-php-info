0639e4aa61fee86b24b109947365199696a7c9a2
Daniel Mikusa <dmikusa@gopivotal.com>
2014-08-05 11:44:03 -0400

    Write cf push log files to file instead of screen.

--- test-push.sh
+++ test-push.sh
@@ -6,18 +6,21 @@ set -e
 git co .bp-config/options.json
 
 # push with Nginx
+echo "Testing Nginx Configuration"
 cp .bp-config/options-nginx.json .bp-config/options.json
-cf push
+cf push > logs/nginx.log
 ./test-extensions.sh 5.4.31
 
 # push with HTTPD
+echo "Testing HTTPD Configuration"
 cp .bp-config/options-httpd.json .bp-config/options.json
-cf push
+cf push > logs/httpd.log
 ./test-extensions.sh 5.4.31
 
 # push with PHP 5.5
+echo "Testing PHP 5.5 Configuration"
 cp .bp-config/options-php55.json .bp-config/options.json
-cf push
+cf push > logs/php55.log
 ./test-extensions.sh 5.5.15
 
 # return to default
